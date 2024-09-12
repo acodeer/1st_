2caaba656bb10f8b9d7c932a644804008e497b1c
acodeer <sang8148@kumoh.ac.kr>
2024-09-12 17:34:08 +0900

    sasta

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-def print_hello():
+def print_damn():
     print("Hello, World!")
 def print_KIT():
     print("take me home contry road")
 
 if __name__ == "__main__":
-    print_hello()
+    print_hello(
